0da60dc0606b0662ae3469d2d3b4b5bf20ecfbff
Sveinung Dalatun <sdalatun@gmail.com>
2020-05-08 22:45:52 +0200

    Få blinkeren til å spinna

--- src/Main.elm
+++ src/Main.elm
@@ -1,69 +1,157 @@
 module Main exposing (..)
 
+import Array exposing (Array)
 import Browser
-import Html exposing (Html, button, div, table, td, tr, text)
+import Html exposing (Html, button, div, table, td, text, tr)
 import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
-
-import Array
+import Time
 
 
 ---- MODEL ----
 
 
-x = True
-o = False
+x =
+    True
 
-type alias Row = List Bool
-type alias Grid = List Row
+
+o =
+    False
+
+
+type alias Row =
+    Array Bool
+
+
+type alias Grid =
+    Array Row
+
+
+type alias Model =
+    { grid : Grid
+    , running : Bool
+    }
+
+
+toArray : List (List Bool) -> Grid
+toArray listGrid =
+    listGrid
+        |> List.map Array.fromList
+        |> Array.fromList
+
+
+toList : Grid -> List (List Bool)
+toList listGrid =
+    listGrid
+        |> Array.map Array.toList
+        |> Array.toList
+
 
 blinker : Grid
-blinker = [
-    [o, o, o, o, o],
-    [o, o, o, o, o],
-    [o, x, x, x, o],
-    [o, o, o, o, o],
-    [o, o, o, o, o]
+blinker =
+    toArray [ [ o, o, o, o, o ]
+    , [ o, o, o, o, o ]
+    , [ o, x, x, x, o ]
+    , [ o, o, o, o, o ]
+    , [ o, o, o, o, o ]
     ]
 
-init : ( Grid, Cmd Msg )
+
+init : ( Model, Cmd Msg )
 init =
-    ( blinker, Cmd.none )
+    ( { grid = blinker
+      , running = False
+      }
+    , Cmd.none
+    )
+
 
 
 ---- UPDATE ----
 
 
-type Msg = Increment | Decrement
+type Msg
+    = Tick Time.Posix
+
 
 getRow : Grid -> Int -> Row
 getRow grid rowIndex =
     let
-        cellRow = Array.fromList grid
-            |> Array.get rowIndex
+        cellRow =
+            grid
+                |> Array.get rowIndex
     in
-        case cellRow of
-            Nothing -> []
-            Just value -> value
+    case cellRow of
+        Nothing ->
+            Array.empty
 
-getCell : Grid ->  Int -> Int -> Bool
+        Just value ->
+            value
+
+
+getCell : Grid -> Int -> Int -> Bool
 getCell grid rowIndex columnIndex =
     let
-        cell = getRow grid rowIndex
-            |> Array.fromList
-            |> Array.get columnIndex
+        cell =
+            getRow grid rowIndex
+                |> Array.get columnIndex
     in
-        case cell of
-            Nothing -> False
-            Just value -> value
+    case cell of
+        Nothing ->
+            False
+
+        Just value ->
+            value
+
+
+iterateCell : Grid -> Int -> (Int, Bool) -> Bool
+iterateCell grid rowIndex (columnIndex, alive) =
+    let
+        getCellInGrid = getCell grid
+        livingNeighbours =
+            [ getCellInGrid (rowIndex - 1) (columnIndex - 1)
+            , getCellInGrid (rowIndex - 1) (columnIndex)
+            , getCellInGrid (rowIndex - 1) (columnIndex + 1)
+            , getCellInGrid (rowIndex) (columnIndex - 1)
+            , getCellInGrid (rowIndex) (columnIndex + 1)
+            , getCellInGrid (rowIndex + 1) (columnIndex - 1)
+            , getCellInGrid (rowIndex + 1) (columnIndex)
+            , getCellInGrid (rowIndex + 1) (columnIndex + 1)
+            ]
+            |> List.filter identity
+        numberOfLivingNeighbours = List.length livingNeighbours
+    in
+    if alive then
+        numberOfLivingNeighbours == 2 || numberOfLivingNeighbours == 3
+    else
+        numberOfLivingNeighbours == 3
+
+
+iterateRow : Grid -> (Int, Row) -> Row
+iterateRow grid (rowIndex, row) =
+    row
+        |> Array.toIndexedList
+        |> List.map (iterateCell grid rowIndex)
+        |> Array.fromList
+
 
 iterate : Grid -> Grid
 iterate grid =
     grid
+        |> Array.toIndexedList
+        |> List.map (iterateRow grid)
+        |> Array.fromList
 
-update : Msg -> Grid -> ( Grid, Cmd Msg )
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( blinker, Cmd.none )
+    case msg of
+        Tick _ ->
+            ( { grid = iterate model.grid
+            , running = True
+            }
+            , Cmd.none )
+
 
 
 ---- VIEW ----
@@ -71,30 +159,52 @@ update msg model =
 
 htmlCell : Bool -> Html msg
 htmlCell alive =
-    td [ class (if alive then "alive cell" else "cell")] []
+    td
+        [ class
+            (if alive then
+                "alive cell"
 
-htmlRow : Row -> Html msg
+             else
+                "cell"
+            )
+        ]
+        []
+
+
+htmlRow : List Bool -> Html msg
 htmlRow row =
     tr []
         (row |> List.map htmlCell)
 
-view : Grid -> Html Msg
-view grid =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , table [] (grid |> List.map htmlRow)
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+
+view : Model -> Html Msg
+view model =
+    let
+        listGrid = toList model.grid
+    in
+    div [ class "app" ]
+        [ table [] (listGrid |> List.map htmlRow)
+        ]
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Time.every 1000 Tick
+
 
 
 ---- PROGRAM ----
 
 
-main : Program () Grid Msg
+main : Program () Model Msg
 main =
     Browser.element
         { view = view
         , init = \_ -> init
         , update = update
-        , subscriptions = always Sub.none
+        , subscriptions = subscriptions
         }
